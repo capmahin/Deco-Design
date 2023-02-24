99760238562e38813c8a9eaef9a556ce9d07e57a
capmahin <mahinulhuque1996@gmail.com>
2023-02-25 00:19:54 +0600

    add all extaintions

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1 className="bg-black text-red text-white text-4xl font-bold">Welcome to Deco-Design</h1>
+    </>
+  )
+}
+
+export default App
